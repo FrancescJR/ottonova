2dcd4cc6564b0b35c14d0d00c475f9de68ee4eb9
fransjr <francesc@xceed.me>
2020-11-16 16:13:19 +0100

    adds exception to interface

--- src/Domain/Employee/EmployeeRepositoryInterface.php
+++ src/Domain/Employee/EmployeeRepositoryInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Cesc\Ottivio\Domain\Employee;
 
 
+use Cesc\Ottivio\Domain\Employee\Exception\EmployeeNotFoundException;
 use Cesc\Ottivio\Domain\Employee\ValueObject\EmployeeName;
 
 interface EmployeeRepositoryInterface
@@ -17,6 +18,7 @@ interface EmployeeRepositoryInterface
      * @param EmployeeName $employeeName
      *
      * @return Employee
+     * @throws EmployeeNotFoundException
      */
     public function getWithContract(EmployeeName $employeeName):Employee;
 
